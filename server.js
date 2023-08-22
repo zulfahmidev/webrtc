c6f1fac2b0a198bccd625eb09e7f8659d4b124c2
zulfahmidev <developer.zulfahmi@gmail.com>
2023-08-22 11:47:35 +0700

    memperbaik ws_uri

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ let argv = minimist(process.argv.slice(2), {
   default: {
     as_url: 'http://localhost:3000',
     // ws_uri: 'ws://localhost:8888/kurento',
-    ws_uri: 'ws://https://kurento-cc5qplbn3a-uc.a.run.app:8888/kurento',
+    ws_uri: 'ws://kurento-cc5qplbn3a-uc.a.run.app/kurento',
   }
 })
 
